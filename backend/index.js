2c36418afbebee34fe7b9fe498095f94d4ca805c
khushi1k4 <khushigoyal2525@gmail.com>
2026-04-01 21:24:38 +0530

    connect frontend with deployed backend

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://smartplatter-meal4all.netlify.app/",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/AuthRoutes"));
